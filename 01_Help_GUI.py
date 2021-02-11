423b84da07bb5d8be1af74a1730a55e971126ca1
janifn9449 <78511947+janifn9449@users.noreply.github.com>
2021-02-12 10:18:08 +1300

    Update 01_Help_GUI.py

--- 01_Help_GUI.py
+++ 01_Help_GUI.py
@@ -1,15 +1,32 @@
 from tkinter import *
+from functools import partial   # to prevent unwanted windows
+
 import random
 
 
-class Foo:
-    def __init__(self, parent):
-        print("hello world")
+class Converter:
+    def __init__(self):
+
+        # formatting variables
+        background_color = "pink"
+
+        # converter main screen GUI...
+        self.converter_frame = Frame(width=600, height=600, bg=background_color)
+        self.converter_frame.grid()
+
+        # Temperature Conversion Heading (row 0)
+        self.temp_converter_label = Label(text="Temperature Converter",
+                                          bg=background_color,
+                                          font=("Arial", "16", "bold"),
+                                          padx=10, pady=10
+                                          )
+        self.temp_converter_label.grid(row=0)
 
 
 # main routine
 if __name__ == "__main__":
     root = Tk()
-    root.title("title goes here")
-    something = Foo(root)
+    root.title("Temperature Converter")
+    something = Converter()
     root.mainloop()
+
